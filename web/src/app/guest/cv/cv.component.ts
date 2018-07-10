4b1388aa529b3f076f22e26ec9080badb4cc31f8
Sanjeev Maharjan <sanjeev.maharjan@hotmail.com>
2018-07-10 19:23:58 +0545

    Cv component added

--- web/src/app/guest/cv/cv.component.ts
+++ web/src/app/guest/cv/cv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbTabChangeEvent} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-cv',
@@ -12,4 +13,9 @@ export class CvComponent implements OnInit {
   ngOnInit() {
   }
 
+  public beforeChange($event: NgbTabChangeEvent) {
+    if ($event.nextId === 'tab-preventchange2') {
+      $event.preventDefault();
+    }
+  };
 }
